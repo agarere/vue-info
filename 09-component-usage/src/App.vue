5e4e0db98fa1a844aa79092742eed4a58ec333c9
Ediz Agarer <ediz.agarer@teamdefinex.com>
2023-09-18 17:55:25 +0300

    Added slot usage

--- 09-component-usage/src/App.vue
+++ 09-component-usage/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-md-6">
+        <ProductCard>
+          <h1 slot="header">Telefon</h1>
+          <p slot="content">IPhone 14 Pro Max</p>
+        </ProductCard>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './BaseCard'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ProductCard
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 09-component-usage/src/BaseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card-wrapper">
+    <div>
+      <slot name="header"></slot>
+    </div>
+    <div>
+      <slot name="content"></slot>
+    </div>
+    <div>
+      <slot name="producer">
+        <!-- here is default shown -->
+        <i>Üretici bilgisi belli değil !</i>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div.card-wrapper {
+  border: 1 solid #cccccc;
+  background: #e0e0e0;
+  border-radius: 16px;
+  padding: 20px 20px;
+  margin-top: 20px;
+}
+</style>
