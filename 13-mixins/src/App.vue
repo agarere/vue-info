1f36c52dff1f5eab67ca2fd3bd14525eba6dfc13
agarere <edizagarer@gmail.com>
2024-01-17 10:07:08 +0300

    Added mixin example

---
+++ 13-mixins/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>myData: {{ myData }}</p>
+    <p>sum: {{ sum (2, 2) }}</p>
+  </div>
+</template>
+
+<script>
+import myMixin from './myMixin';
+
+export default {
+  name: 'App',
+
+  mixins: [
+    myMixin
+  ]
+}
+</script>
---
+++ 13-mixins/src/myMixin.js
@@ -0,0 +1,13 @@
+export default {
+  data () {
+    return {
+      myData: 10
+    }
+  },
+
+  methods: {
+    sum (x, y) {
+      return Number(x) + Number(y)
+    }
+  },
+}
